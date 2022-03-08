a4a3607cc4d7f926d5d1cc0a0cf94d99efa042fc
wangfan01 <wangfan01@kaikeba.com>
2022-03-08 15:46:31 +0800

    feat: 修改url

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env sh
-
+ 
 # 确保脚本抛出遇到的错误
 set -e
-
+ 
 # 生成静态文件
 npm run docs:build
-
+ 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+ 
 git init
 git add -A
 git commit -m 'deploy'
-
-# dist目录发布到 https://xiumubai.github.io
-# git push -f将本地的代码库推送到远端，并覆盖
-git push -f git@github.com:songzi0321/songzi.github.io.git main
+ 
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:songzi0321/songzi.github.io.git main:gh-pages
+ 
+cd -
