3e142fa1639718cdfc00da2ecd4e45d99c97a2c5
wangfan01 <wangfan01@kaikeba.com>
2022-03-08 15:52:10 +0800

    feat: 修改base

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
  
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:songzi0321/songzi.github.io.git main:gh-pages
+git push -f git@github.com:songzi0321/songzi.github.io.git main
  
 cd -
